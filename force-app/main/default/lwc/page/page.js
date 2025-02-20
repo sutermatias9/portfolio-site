90f51ec1451ea7e1efc384d7d191ffb90c5d4506
Matias Suter <sutermatias9@gmail.com>
2025-02-19 21:01:32 -0300

    Add project tiles

--- force-app/main/default/lwc/page/page.js
+++ force-app/main/default/lwc/page/page.js
@@ -5,4 +5,9 @@ export default class Page extends LightningElement {
   bannerImage = IMAGES + "/static_images/images/hero.png";
   githubIcon = IMAGES + "/static_images/images/github_icon.png";
   linkedinIcon = IMAGES + "/static_images/images/linkedin_icon.png";
+  playIcon = IMAGES + "/static_images/images/play_icon.png";
+  shoppingScreenshot =
+    IMAGES + "/static_images/images/shopping-cart-screenshot.png";
+  coverageScreenshot =
+    IMAGES + "/static_images/images/class-coverage-screenshot.png";
 }
