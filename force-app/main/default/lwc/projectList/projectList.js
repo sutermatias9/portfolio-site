b112912c8e33faf05817c70d5543cdecb9659494
Matias Suter <sutermatias9@gmail.com>
2025-02-24 12:10:49 -0300

    Modularize page: add aboutMe and projectList LWC

---
+++ force-app/main/default/lwc/projectList/projectList.js
@@ -0,0 +1,21 @@
+import { LightningElement } from "lwc";
+import IMAGES from "@salesforce/resourceUrl/imgs";
+
+export default class ProjectList extends LightningElement {
+  // Project screenshots
+  shoppingScreenshot =
+    IMAGES + "/static_images/images/shopping-cart-screenshot.png";
+  coverageScreenshot =
+    IMAGES + "/static_images/images/class-coverage-screenshot.png";
+  examScreenshot =
+    IMAGES + "/static_images/images/exam-simulator-screenshot.png";
+
+  // Tile button images
+  playIcon = IMAGES + "/static_images/images/play_icon.png";
+  githubIcon = IMAGES + "/static_images/images/github_icon.png";
+
+  handleRedirect(event) {
+    const url = event.currentTarget.dataset.url;
+    window.open(url, "_blank");
+  }
+}
