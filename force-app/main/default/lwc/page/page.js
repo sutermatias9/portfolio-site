1c05522be4e92386fa2ba90de967e8358bc59254
Matias Suter <sutermatias9@gmail.com>
2025-02-25 14:11:30 -0300

    Show toast on email sent

--- force-app/main/default/lwc/page/page.js
+++ force-app/main/default/lwc/page/page.js
@@ -1,13 +1,33 @@
-import { LightningElement } from "lwc";
-import IMAGES from "@salesforce/resourceUrl/imgs";
+import { LightningElement } from 'lwc';
+import ToastContainer from 'lightning/toastContainer';
+import Toast from 'lightning/toast';
+
+import IMAGES from '@salesforce/resourceUrl/imgs';
 
 export default class Page extends LightningElement {
-  bannerImage = IMAGES + "/static_images/images/hero.png";
-  linkedinIcon = IMAGES + "/static_images/images/linkedin_icon.png";
-  githubIcon = IMAGES + "/static_images/images/github_icon.png";
+    bannerImage = IMAGES + '/static_images/images/hero.png';
+    linkedinIcon = IMAGES + '/static_images/images/linkedin_icon.png';
+    githubIcon = IMAGES + '/static_images/images/github_icon.png';
 
-  handleRedirect(event) {
-    const url = event.currentTarget.dataset.url;
-    window.open(url, "_blank");
-  }
+    connectedCallback() {
+        const toastContainer = ToastContainer.instance();
+        toastContainer.maxToasts = 3;
+        toastContainer.toastPosition = 'top-center';
+    }
+
+    handleEmailSent(event) {
+        const { isSuccess, error } = event.detail;
+
+        if (isSuccess) this.showToast('Email sent successfully!', null, 'success');
+        else this.showToast('Failed to send email. Please try again later.', null, 'error'); // falta reduceError();
+    }
+
+    handleRedirect(event) {
+        const url = event.currentTarget.dataset.url;
+        window.open(url, '_blank');
+    }
+
+    showToast(label, message, variant) {
+        Toast.show({ label, message, variant });
+    }
 }
