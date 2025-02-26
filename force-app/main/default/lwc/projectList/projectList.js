6e8063665e7089d836317d53564dfce3df6a9a52
Matias Suter <sutermatias9@gmail.com>
2025-02-26 18:51:57 -0300

    Add Portfolio Site to project list
    - Change cursor to default on skill text hover
    - Update role font

--- force-app/main/default/lwc/projectList/projectList.js
+++ force-app/main/default/lwc/projectList/projectList.js
@@ -1,21 +1,19 @@
-import { LightningElement } from "lwc";
-import IMAGES from "@salesforce/resourceUrl/imgs";
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/images';
 
 export default class ProjectList extends LightningElement {
-  // Project screenshots
-  shoppingScreenshot =
-    IMAGES + "/static_images/images/shopping-cart-screenshot.png";
-  coverageScreenshot =
-    IMAGES + "/static_images/images/class-coverage-screenshot.png";
-  examScreenshot =
-    IMAGES + "/static_images/images/exam-simulator-screenshot.png";
+    // Project screenshots
+    shoppingScreenshot = IMAGES + '/static_images/images/shopping-cart-screenshot.png';
+    coverageScreenshot = IMAGES + '/static_images/images/class-coverage-screenshot.png';
+    examScreenshot = IMAGES + '/static_images/images/exam-simulator-screenshot.png';
+    portfolioScreenshot = IMAGES + '/static_images/images/portfolio-screenshot.png';
 
-  // Tile button images
-  playIcon = IMAGES + "/static_images/images/play_icon.png";
-  githubIcon = IMAGES + "/static_images/images/github_icon.png";
+    // Tile button images
+    playIcon = IMAGES + '/static_images/images/play_icon.png';
+    githubIcon = IMAGES + '/static_images/images/github_icon.png';
 
-  handleRedirect(event) {
-    const url = event.currentTarget.dataset.url;
-    window.open(url, "_blank");
-  }
+    handleRedirect(event) {
+        const url = event.currentTarget.dataset.url;
+        window.open(url, '_blank');
+    }
 }
